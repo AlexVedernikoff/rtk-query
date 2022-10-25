00fbeef43aa10b13599f53c1e80f1581f8b22a40
AlexVedernikoff <templar_sc@mail.ru>
2022-10-25 07:13:42 +0300

    перед typeScript

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
-import {   useDeleteMedicamentByIdMutation, 
-  useGetMedicamentByIdQuery, 
-  useGetMedicamentsListQuery, 
+import {
+  useDeleteMedicamentByIdMutation,
+  useGetMedicamentByIdQuery,
+  useGetMedicamentsListQuery,
   useCreateMedicamentMutation,
   useModifyMedicamentMutation
-  } from "./redux";
+} from "./redux";
 import { useState } from "react";
 
 export default function App() {
@@ -13,90 +14,90 @@ export default function App() {
   const [searchId, setSearchId] = useState("");
   const [modifyId, setModifyId] = useState("");
   const token =
-    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYW5hZ2VyMUBlbWFpbC5jb20iLCJpYXQiOjE2NjYwMTYyNDMsImV4cCI6MTY2ODAxNjI0M30.vrTJfq_xvFPRA3-Z_bcc69ie9Kjo-MfXekpyEdgS7UiCbty28mfP8zuaqw-QsevEwVaQoySryaZCGrFyeO7Ufw";
+    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYW5hZ2VyMUBlbWFpbC5jb20iLCJpYXQiOjE2NjY2MjMyMDAsImV4cCI6MTY2ODYyMzIwMH0.34oeXFhnwf4OACHCdZC6ojlN3JaaRNLqtUKcfGyFZJEzUQVNLZ0MD5ZskReGv-hzbN2B4MAy_iFXDxMEbwQSkg";
   localStorage.setItem("token", token);
-  const {data: medicamentById } = useGetMedicamentByIdQuery(searchId);
-  const {data: medicamentsList } = useGetMedicamentsListQuery();
-  
+  const { data: medicamentById } = useGetMedicamentByIdQuery(searchId);
+  const { data: medicamentsList } = useGetMedicamentsListQuery();
+
   const [createMedicament] = useCreateMedicamentMutation();
-  const [deleteMedicament, {error: deleteError}] = useDeleteMedicamentByIdMutation();
-  const [modifyMedicament, {error: modifyError}] = useModifyMedicamentMutation();
-  
-  const bodyRequest = 
-    {
-      "manufactureName": "MosKhimProm",
-      "name": addName,
-      "iconUrl": "icon1",
-      "description": "ot vsego"
-    }
+  const [deleteMedicament, { error: deleteError }] = useDeleteMedicamentByIdMutation();
+  const [modifyMedicament, { error: modifyError }] = useModifyMedicamentMutation();
+
+  const bodyRequest =
+  {
+    "manufactureName": "MosKhimProm",
+    "name": addName,
+    "iconUrl": "icon1",
+    "description": "ot vsego"
+  }
 
   const handleCreateMedicament = async () => {
-      await createMedicament(bodyRequest)
+    await createMedicament(bodyRequest)
   }
 
   const handleDeleteMedicament = async () => {
-      await deleteMedicament(deleteId)
+    await deleteMedicament(deleteId)
   }
 
   const handleModifyMedicament = async () => {
     console.log("bodyRequest = ", bodyRequest)
     console.log("modifyId = ", modifyId)
-    console.log({bodyRequest, modifyId})
-    await modifyMedicament({bodyRequest, modifyId})
+    console.log({ bodyRequest, modifyId })
+    await modifyMedicament({ bodyRequest, modifyId })
   }
 
- if (medicamentsList) {
-  console.log("MedicamentsList = ", " ", medicamentsList);
- }
- if (deleteError) {
-  console.log("Ошибка удаления! = ", deleteError)
- }
+  if (medicamentsList) {
+    console.log("MedicamentsList = ", " ", medicamentsList);
+  }
+  if (deleteError) {
+    console.log("Ошибка удаления! = ", deleteError)
+  }
 
- 
+
 
   return (
     <div className="App">
-      <div className = "input">
-        <input 
+      <div className="input">
+        <input
           type="text"
-          value = {searchId}
-          onChange = {(event)=>{setSearchId(event.target.value)}}
+          value={searchId}
+          onChange={(event) => { setSearchId(event.target.value) }}
         />
-        <button onClick = {()=>console.log("MedicamentById = ", " ", medicamentById)}>Поиск медикамента по Id</button>
+        <button onClick={() => console.log("MedicamentById = ", " ", medicamentById)}>Поиск медикамента по Id</button>
       </div>
-      <div className = "input">
-        <button onClick = {()=>console.log("MedicamentsList = ", " ", medicamentsList)}>Вывести список препаратов</button>
+      <div className="input">
+        <button onClick={() => console.log("MedicamentsList = ", " ", medicamentsList)}>Вывести список препаратов</button>
       </div>
-      <div className = "input">
-        <input 
+      <div className="input">
+        <input
           type="text"
-          value = {deleteId}
-          onChange = {(event)=>{setDeleteId(event.target.value)}}
+          value={deleteId}
+          onChange={(event) => { setDeleteId(event.target.value) }}
         />
-        <button onClick = {handleDeleteMedicament}>Удалить медикамент</button>
+        <button onClick={handleDeleteMedicament}>Удалить медикамент</button>
       </div>
-      <div className = "input">
-        <input 
+      <div className="input">
+        <input
           type="text"
-          value = {addName}
-          onChange = {(event)=>{setAddName(event.target.value)}}
+          value={addName}
+          onChange={(event) => { setAddName(event.target.value) }}
         />
-        <button onClick = {handleCreateMedicament}>Добавить медикамент</button>
+        <button onClick={handleCreateMedicament}>Добавить медикамент</button>
       </div>
-      <div className = "input">
-        <input 
+      <div className="input">
+        <input
           type="text"
           placeholder="id"
-          value = {modifyId}
-          onChange = {(event)=>{setModifyId(event.target.value)}}
+          value={modifyId}
+          onChange={(event) => { setModifyId(event.target.value) }}
         />
-        <input 
+        <input
           type="text"
           placeholder="name"
-          value = {addName}
-          onChange = {(event)=>{setAddName(event.target.value)}}
+          value={addName}
+          onChange={(event) => { setAddName(event.target.value) }}
         />
-        <button onClick = {handleModifyMedicament}>Изменить медикамент</button>
+        <button onClick={handleModifyMedicament}>Изменить медикамент</button>
       </div>
     </div>
   );
